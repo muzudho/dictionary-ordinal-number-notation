6154fa602b1a100e136af9d72c37657fd151dcb4
むずでょ <muzudho1@gmail.com>
2022-07-21 18:45:53 +0900

    負数への対応

--- src/dicordnum/__init__.py
+++ src/dicordnum/__init__.py
@@ -4,13 +4,13 @@ import re
 class DicOrdNum:
     """Dictionary Ordinal Number"""
 
-    pattern = re.compile(r"([Aa]*)(\d+)")
+    pattern = re.compile(r"([Aa]*|[_]*)([0-9]+)")
 
     def __init__(self, value=0):
 
         try:
             # 整数かどうか判定
-            num = int(str(value), 10)
+            int_value = int(str(value), 10)
         except ValueError:
             # 整数ではなかった
             result = DicOrdNum.pattern.match(value)
@@ -31,19 +31,44 @@ class DicOrdNum:
                 raise ValueError(f"not dictionary ordinal number: {value}")
         else:
             # 整数だ
-            if num < 0:
-                # 負数だ
-                raise ValueError(f"not dictionary ordinal number: {value}")
-            else:
-                # 正の数だ
-                self._number = value
+            self._number = int_value
 
     def __str__(self):
-        figure = len(str(self._number))
-        prefix = ""
-        for i in range(1, figure):
-            prefix += "A"
-        return f"{prefix}{self._number}"
+        if self._number < 0:
+            # 負の整数
+            #
+            # "-1" の絶対値 "1" なら文字列の桁数は 1
+            figure = len(str(abs(self._number)))
+
+            # Underscore
+            prefix_u = ""
+            for i in range(0, figure):
+                prefix_u += "_"
+
+            # その負数の絶対値より１桁大きい数の中で一番小さな数
+            modulo = 1
+            for i in range(0, figure):
+                modulo *= 10
+
+            # 差
+            diff_num = modulo + self._number
+            # 前ゼロの桁数 = モジュロの桁数 - 差の桁数 - 1
+            prefix_z_figure = len(str(modulo)) - len(str(diff_num)) - 1
+
+            # 前ゼロ
+            prefix_z = ""
+            for i in range(0, prefix_z_figure):
+                prefix_z += "0"
+
+            return f"{prefix_u}{prefix_z}{diff_num}"
+
+        else:
+            # 零 or 正の整数
+            figure = len(str(self._number))
+            prefix = ""
+            for i in range(1, figure):
+                prefix += "A"
+            return f"{prefix}{self._number}"
 
     @property
     def number(self):
--- tests/test.py
+++ tests/test.py
@@ -4,46 +4,65 @@ python -m tests.test
 from src.dicordnum import DicOrdNum
 
 test_data = [
+    # 正の零
+    ['0', DicOrdNum(0), '0'],
+    # --                ---
+    # 1                 2
+    # 1. Actual
+    # 2. Expected
+
     # 正の整数
-    ['0', DicOrdNum(0)],
-    ['9', DicOrdNum(9)],
-    ['10', DicOrdNum(10)],
-    ['99', DicOrdNum(99)],
-    ['100', DicOrdNum(100)],
-    ['999', DicOrdNum(999)],
-    ['1000', DicOrdNum(1000)],
+    ['9', DicOrdNum(9), '9', '9'],
+    ['10', DicOrdNum(10), 'A10'],
+    ['99', DicOrdNum(99), 'A99'],
+    ['100', DicOrdNum(100), 'AA100'],
+    ['999', DicOrdNum(999), 'AA999'],
+    ['1000', DicOrdNum(1000), 'AAA1000'],
+
+    # 負の零（正の零扱い）
+    ['-0', DicOrdNum(-0), '0'],
+
+    # 負の整数
+    ['-1', DicOrdNum(-1), '_9'],
+    ['-2', DicOrdNum(-2), '_8'],
+    ['-9', DicOrdNum(-9), '_1'],
+    ['-10', DicOrdNum(-10), '__90'],
+    ['-99', DicOrdNum(-99), '__01'],
+    ['-100', DicOrdNum(-100), '___900'],
+    ['-999', DicOrdNum(-999), '___001'],
+    ['-1000', DicOrdNum(-1000), '____9000'],
+    ['-9999', DicOrdNum(-9999), '____0001'],
 
     # 正の整数の文字列
-    ['"0"', DicOrdNum('0')],
-    ['"9"', DicOrdNum('9')],
-    ['"10"', DicOrdNum('10')],
-    ['"99"', DicOrdNum('99')],
-    ['"100"', DicOrdNum('100')],
-    ['"999"', DicOrdNum('999')],
-    ['"1000"', DicOrdNum('1000')],
+    ['"0"', DicOrdNum('0'), '0'],
+    ['"9"', DicOrdNum('9'), '9'],
+    ['"10"', DicOrdNum('10'), 'A10'],
+    ['"99"', DicOrdNum('99'), 'A99'],
+    ['"100"', DicOrdNum('100'), 'AA100'],
+    ['"999"', DicOrdNum('999'), 'AA999'],
+    ['"1000"', DicOrdNum('1000'), 'AAA1000'],
 
     # 辞書順記数法
-    ['"A77"', DicOrdNum('A77')],
-    ['"AA777"', DicOrdNum('AA777')],
-    ['"AAA7777"', DicOrdNum('AAA7777')],
-    ['"AAAA77777"', DicOrdNum('AAAA77777')],
+    ['"A77"', DicOrdNum('A77'), 'A77'],
+    ['"AA777"', DicOrdNum('AA777'), 'AA777'],
+    ['"AAA7777"', DicOrdNum('AAA7777'), 'AAA7777'],
+    ['"AAAA77777"', DicOrdNum('AAAA77777'), 'AAAA77777'],
 
     # 小文字, snake_case への寛容
-    ['"a77"', DicOrdNum('a77')],
-    ['"aa777"', DicOrdNum('aa777')],
-    ['"aaa7777"', DicOrdNum('aaa7777')],
-    ['"aaaa77777"', DicOrdNum('aaaa77777')],
+    ['"a77"', DicOrdNum('a77'), 'A77'],
+    ['"aa777"', DicOrdNum('aa777'), 'AA777'],
+    ['"aaa7777"', DicOrdNum('aaa7777'), 'AAA7777'],
+    ['"aaaa77777"', DicOrdNum('aaaa77777'), 'AAAA77777'],
 ]
 
 for datum in test_data:
-    print(f"{datum[0]} --> {datum[1]} {datum[1].number}")
+    if f"{datum[1]}" == datum[2]:
+        print(f"{datum[0]} --> {datum[1]} {datum[1].number}")
+    else:
+        print(
+            f"[Error] {datum[0]} --> {datum[1]} {datum[1].number} Expected: {datum[2]}")
 
-try:
-    print(f"[Error] -1 --> {DicOrdNum('-1')}")
-except:
-    print(f"-1 is not DicOrdNum")
-
-# Aの個数が合っているか確認します
+# 間違ったAの個数を例外にできるか確認します
 try:
     print(f"[Error] A1 --> {DicOrdNum('A1')}")
 except:
